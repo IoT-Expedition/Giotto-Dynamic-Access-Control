64e3eff8c857070f80a46553e4599051306f0ef4
gokulkrishnaa <gokul.krishnaa1@gmail.com>
2016-05-04 21:33:16 -0400

    Dynamic Access Control

--- Giotto/app/src/main/java/com/example/giotto/MainActivity.java
+++ Giotto/app/src/main/java/com/example/giotto/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends Activity{
         pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
         editor = pref.edit();
 
-        ip = pref.getString("ip","http://128.2.113.192:82");
+        ip = pref.getString("ip","http://cmu.buildingdepot.org:82");
 
         final EditText user_name = (EditText)  findViewById(R.id.user_name);
         user_name.setText(pref.getString("user","admin@admin.com"));
